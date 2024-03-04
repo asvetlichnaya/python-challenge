a48b75a5a0b84a0e88a48d51aa6c34a371c9ef75
Anna Svitlychna <annsvetlichny@gmail.com>
2024-03-04 14:50:03 +0100

    Added task5

--- task4.py
+++ task4.py
@@ -1,8 +1,9 @@
 # Write a word counter: as input program accepts a txt file, then reads the content from the file and as
 # output prints the number of words in that txt file.
 # You need to create this txt file first and fill it with some sentences.
+import re
 
 with open('files/input_file.txt') as f:
     content = f.read()
     print(content)
-    print('There are ', len(content.split()), ' words')
+    print('There are ', len(re.split(r'\W+', content)), ' words')
---
+++ task5.py
@@ -0,0 +1,17 @@
+# Write a function to check duplicate letters in words. It must accept strings i.e. a sentence.
+# The function should return True if any of the words has duplicate letters else return False.
+import re
+
+
+def if_duplicate_letters_exists(value: str, flag=False):
+    split_string = re.split(r'\W+', value)
+    for word in split_string:
+        for letter in word:
+            if letter in word[word.index(letter) + 1:len(word):1]:
+                flag = True
+                break
+    return flag
+
+
+input_string = input('Please input a string: ').strip()
+print(if_duplicate_letters_exists(input_string))
