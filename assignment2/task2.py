980833c47d806ad60e8980fed2500e161fd18b41
Anna Svitlychna <annsvetlichny@gmail.com>
2024-03-09 21:33:15 +0100

    Added task6

--- assignment2/task2.py
+++ assignment2/task2.py
@@ -5,7 +5,7 @@
 # You need to create this txt file first and fill it with some sentences.
 import re
 
-with open('files/input_file.txt') as f:
+with open('input_files/input_file.txt') as f:
     content = f.read()
     print(content)
     print('There are ', len(re.split(r'\W+', content)), ' words')
