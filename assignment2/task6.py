16c8db4f7a38941513f37855611bbd91c37caee9
Anna Svitlychna <annsvetlichny@gmail.com>
2024-03-11 18:37:15 +0100

    Added task6, 7

--- assignment2/task6.py
+++ assignment2/task6.py
@@ -2,21 +2,24 @@ import re
 
 
 def count_vowels_in_csv_file(text: str):
-    split_string = re.split(r'\W+', text)
-
-    count_of_vowels = {}
-    x = {}
+    split_string = re.split(r'\W+', text.lower().strip())
+    n_vowels_in_str = {}
     for word in split_string:
-        list_of_vowels = re.findall("[aeiouy]", word)
-        x = {}
-        for vowel in list_of_vowels:
-            x[vowel] = list_of_vowels.count(vowel)
-            count_of_vowels[word] = x
-    return str(count_of_vowels)
+        count_of_vowels = {}
+        for letter in word:
+            if letter in {'a', 'e', 'i', 'o', 'u', 'y'}:
+                x = count_of_vowels.get(letter)
+                if x is None:
+                    count_of_vowels[letter] = 1
+                else:
+                    count_of_vowels[letter] = x + 1
+        n_vowels_in_str[word] = count_of_vowels
+    return str(n_vowels_in_str)
 
 
-with open('input_files/inputs.csv') as f:
-    content = f.read()
+with open('input_files/inputs.csv') as file:
+    content = file.read()
+    print(count_vowels_in_csv_file(content))
 
-with open('output_files/output.txt', 'w') as f:
-    f.write(count_vowels_in_csv_file(content))
+with open('output_files/output.txt', 'w') as file:
+    file.write(count_vowels_in_csv_file(content))
